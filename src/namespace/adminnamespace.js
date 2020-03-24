d96a88f7b88b2dfa8c07995eb035627ce14fc92c
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-03-24 13:46:40 +0100

    Admin and custom namespace created

--- src/namespace/adminnamespace.js
+++ src/namespace/adminnamespace.js
@@ -1,25 +1,31 @@
-import {SocketConfig, logger} from './conf.js';
+import {logger} from '../conf/config.js';
 import {CustomNamespace} from './customnamespace.js'
 
+/**
+ * This class implements an AdminNamespace that extends a CustomNamespace
+ */
 export class AdminNamespace extends CustomNamespace {
+    /**
+     * Default constructor that saves the socket.io Namespace
+     * @param nsp socket.io Namespace
+     */
     constructor(nsp) {
         super(nsp);
     }
 
+    /**
+     * Starts handling for events.
+     */
     startSocketHandling() {
         this.nsp.on('connection', this.onConnection);
     }
 
+    /**
+     * Function that handles the new socket connection.
+     * @param socket new connected socket.
+     */
     onConnection(socket) {
         logger.debug(`New connected socket (socketid: ${socket.id}, username: ${socket.username})`);
-
-        socket.on('newDebate', (callback) => {
-            logger.info(`New debate creation requested from ${socket.username}`);
-            /* Generate debate id */
-            const idDebate = '1';
-            socket.join(`${SocketConfig.ADMIN_ROOM_PREFIX}-${idDebate}`);
-            callback(idDebate);
-        });
 
         socket.on('disconnect', _ => {
             logger.debug(`socket (${socket.id}) disconnected`);
--- src/namespace/customnamespace.js
+++ src/namespace/customnamespace.js
@@ -2,16 +2,29 @@
  * Class wrapping a custom socket.io Namespace
  */
 export class CustomNamespace {
+    // Socket.io Namespace
     nsp;
 
+    /**
+     * Default constructor that saves the socket.io Namespace
+     * @param nsp socket.io Namespace
+     */
     constructor(nsp) {
         this.nsp = nsp;
     }
 
-    setMiddleware(middleware) {
+    /**
+     * This function configures a new middleware for the namespace.
+     * @param middleware function that takes a socket and a function in argument
+     */
+    registerMiddleware(middleware) {
         this.nsp.use(middleware);
     }
 
+    /**
+     * This function setup the namespace by listening on events and handling them.
+     * It must be implemented by child classes.
+     */
     startSocketHandling() {
         throw new Error('startSocketHandling() is not implemented.');
     }
