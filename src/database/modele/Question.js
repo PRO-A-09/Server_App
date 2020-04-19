c2c9f1b8824cbbefc7befc089b49df4c089d5f82
Naludrag <stephanecarvalho81@gmail.com>
2020-04-19 18:40:16 +0200

    Added inheritance and improved verification when saving data

--- src/database/modele/Question.js
+++ src/database/modele/Question.js
@@ -1,12 +1,14 @@
 /*
 * File: Question.js
 * Authors: Stéphane Teixeira Carvalho
-* Modified by: -
+* Modified by: Stéphane Teixeira Carvalho
 * Created on 26 mars 2020
 * Description: Implement the schema that represents how we want a question
 * to be entered in the MongoDB database
 */
 import mongoose from 'mongoose';
+import {Discussion} from "./Discussion.js";
+import {Tag} from "./Tag.js";
 const QuestionSchema = new mongoose.Schema({
     //Redefinition of the primary key _id to be a Number by default it is a ObjectID
     _id: {
@@ -24,14 +26,27 @@ const QuestionSchema = new mongoose.Schema({
     refDiscussion:{
         type: Number,
         ref: 'Discussion',
-        required: true
+        required: true,
+        //Validate will permit us to make some validation before a refDiscussion is saved
+        //The function check if the id of the value passed exits in the DataBase if yes it will return true otherwise false
+        validate: function(v) {
+            return new Promise(function(resolve, reject) {
+                Discussion.findOne({_id: v}, (err, discussion) => resolve(discussion ? true : false));
+            });
+        }
     },
     //Multiple tags can be linked to a question so declaration of an array
     tags:[
         {
             refTag: {
                 type: Number,
-                ref: 'Tag'
+                ref: 'Tag',
+                //Validate will permit us to make some validation before a refTag is saved
+                validate: function(v) {
+                    return new Promise(function(resolve, reject) {
+                        Tag.findOne({_id: v}, (err, tag) => resolve(tag ? true : false));
+                    });
+                }
             }
         }
     ]
