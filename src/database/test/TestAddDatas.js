e5507a289038e584773984f9a359008fbe485e25
Arthur Bécaud <arthur.becaud@heig-vd.ch>
2020-04-21 09:43:25 +0200

    Update src/database/test/TestAddDatas.js

--- src/database/test/TestAddDatas.js
+++ src/database/test/TestAddDatas.js
@@ -25,7 +25,7 @@ mongoose.connect('mongodb://192.168.99.100:27017/PRO', {useNewUrlParser: true});
  */
 async function test() {
     try {
-        //Creation of a participant and save him in the database
+        // Creation of a participant and save him in the database
         const participant1 = new Participant({
             _id: 1,
             name: 'Bill',
