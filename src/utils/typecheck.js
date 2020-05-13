edeac027ffd3a2bf1972a92991d1008f546f15bf
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-05-13 15:34:07 +0200

    Added isArray and isArrayOf functions

--- src/utils/typecheck.js
+++ src/utils/typecheck.js
@@ -27,3 +27,28 @@ export function isFunction (value) {
 export function isInteger(value) {
     return Number.isInteger(value);
 }
+
+/**
+ * Returns if a value is an array
+ * @param value value to check
+ * @returns {boolean} true if an array, false otherwise
+ */
+export function isArray(value) {
+    return Array.isArray(value);
+}
+
+/**
+ * Returns if a value is an array of a specific type
+ * @param value value to check
+ * @param fnTypeCheck function that checks the type
+ * @param maxSize optional, maximum size of the array to be valid
+ */
+export function isArrayOf(value, fnTypeCheck, maxSize) {
+    if (!isArray(value))
+        return false;
+
+    if (maxSize && value.length > maxSize)
+        return false;
+
+    return value.every((v) => fnTypeCheck(v));
+}
