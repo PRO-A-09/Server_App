14652a491e04fc07ab4278f9cc43af50510e632d
BecaudA <arthur.becaud@heig-vd.ch>
2020-05-28 17:13:55 +0200

    docker_run.sh now update images before running

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Run docker container
-docker-compose build
+docker-compose stop
+docker-compose rm -f
+docker-compose pull
+
 if [ "$1" = "-d" ] ; then
     docker-compose up -d
 else
